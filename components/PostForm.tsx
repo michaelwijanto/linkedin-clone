adc7ed1a44e3eb0d0378eedff35f0def2ce452f1
michaelwijanto <michael.wijanto@gmail.com>
2024-05-20 16:09:46 +0700

    initialize create-post-action

--- components/PostForm.tsx
+++ components/PostForm.tsx
@@ -5,12 +5,32 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
 import { ImageIcon, XIcon } from "lucide-react";
 import { useRef, useState } from "react";
+import createPostAction from "@/actions/createPostAction";
 
 function PostForm() {
   const { user } = useUser();
   const ref = useRef<HTMLFormElement>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [preview, setPreview] = useState<string | null>(null);
+
+  const handlePostAction = async (formData: FormData) => {
+    const formDataCopy = formData;
+    ref.current?.reset();
+
+    const text = formDataCopy.get("postInput") as string;
+
+    if (!text.trim()) {
+      throw new Error("You must provide a post input");
+    }
+
+    setPreview(null);
+
+    try {
+      await createPostAction(formDataCopy);
+    } catch (error) {
+      console.log("Error creating post: ", error);
+    }
+  };
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -20,8 +40,16 @@ function PostForm() {
   };
 
   return (
-    <div>
-      <form ref={ref} action="">
+    <div className="mb-2">
+      <form
+        ref={ref}
+        action={(formData) => {
+          // Handle form submission with server action
+          // Toast notification based on the promise
+          handlePostAction(formData);
+        }}
+        className="p-3 bg-white rounded-lg border"
+      >
         <div className="flex items-center space-x-2">
           <Avatar>
             <AvatarImage src={user?.imageUrl} />
