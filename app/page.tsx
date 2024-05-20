60fd1ac48ba160f84565672e5a451f0c343478f9
michaelwijanto <michael.wijanto@gmail.com>
2024-05-20 16:00:16 +0700

    initial post form

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,21 @@
+import PostForm from "@/components/PostForm";
 import UserInformation from "@/components/UserInformation";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid">
-      <section>
+    <div className="grid gird-cols-8 mt-5 sm:px-5">
+      <section className="hidden md:inline md:col-span-2">
         <UserInformation />
       </section>
 
-      <section>
+      <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
         {/* PostForm */}
+        <PostForm />
         {/* PostFeed */}
       </section>
 
-      <section>
+      <section className="gidden xl:inline justify-center col-span-2">
         {/* Widget */}
       </section>
     </div>
