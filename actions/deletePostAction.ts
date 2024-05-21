b5b1bc701d90525e45bc2d275af8e32029570a10
michaelwijanto <michael.wijanto@gmail.com>
2024-05-21 15:26:29 +0700

    delete post action

--- actions/deletePostAction.ts
+++ actions/deletePostAction.ts
@@ -1,6 +1,7 @@
 "user server";
 import { Post } from "@/mongodb/models/post";
 import { currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
 export default async function deletePostAction(postId: string) {
   const user = await currentUser();
@@ -10,4 +11,19 @@ export default async function deletePostAction(postId: string) {
   }
 
   const post = await Post.findById(postId);
+
+  if (!post) {
+    throw new Error("Post not found");
+  }
+
+  if (post.user.userId !== user.id) {
+    throw new Error("Post does not belong to the user");
+  }
+
+  try {
+    await post.removePost();
+    revalidatePath("/");
+  } catch (error) {
+    throw new Error("An error occurred while deleting the post");
+  }
 }
